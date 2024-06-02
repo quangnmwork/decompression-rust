03464f4af1f97531d0e5c8af38854c1f1101d36e
quangnmwork <quangnm.work@gmail.com>
2024-06-02 13:01:29 +0900

    implement simple decompress func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::{fs, io};
+
+
 fn main() {
-    println!("Hello, world!");
+    let file = fs::File::open("compress3.zip").unwrap();
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
+
+        let outpath = match file.enclosed_name() {
+            Some(path) => path.to_owned(),
+            None => continue
+        };
+
+        if (file.name()).ends_with('/') {
+            println!("File {} extracted to \"{}\"", i, outpath.display());
+
+            fs::create_dir_all(&outpath).unwrap();
+        }
+
+        else {
+            println!(
+                "File {} extracted to \"{}\" ({} bytes)",
+                i,
+                outpath.display(),
+                file.size()
+            );
+
+            let path_parent = outpath.parent();
+
+
+            if !path_parent.unwrap().exists() {
+                fs::create_dir_all(path_parent.unwrap()).unwrap();
+            }
+
+
+            let mut outfile = fs::File::create(&outpath).unwrap();
+
+            io::copy(&mut file,&mut outfile).unwrap();
+        }
+    }
 }
