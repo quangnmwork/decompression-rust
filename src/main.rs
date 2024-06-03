e1e30d45f3f24336d0911585aa8ee3cc50565676
quangnmwork <quangnm.work@gmail.com>
2024-06-03 22:51:42 +0900

    feat: add comment

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,23 @@ fn main() {
     let file = fs::File::open("compress3.zip").unwrap();
     let mut archive = zip::ZipArchive::new(file).unwrap();
 
+    /**
+        There can be multiple files.
+     */
     for i in 0..archive.len() {
         let mut file = archive.by_index(i).unwrap();
 
+        /**
+            Ensure zip file exist
+        */
         let outpath = match file.enclosed_name() {
             Some(path) => path.to_owned(),
             None => continue
         };
 
+        /**
+            If input is a folder path.
+         */
         if (file.name()).ends_with('/') {
             println!("File {} extracted to \"{}\"", i, outpath.display());
 
@@ -29,14 +38,19 @@ fn main() {
 
             let path_parent = outpath.parent();
 
-
+            /** Create parent directory */
             if !path_parent.unwrap().exists() {
                 fs::create_dir_all(path_parent.unwrap()).unwrap();
             }
 
-
+            /**
+                Create out file
+             */
             let mut outfile = fs::File::create(&outpath).unwrap();
 
+            /**
+                Copy content file in zip folder to outfile
+             */
             io::copy(&mut file,&mut outfile).unwrap();
         }
     }
